0e0970a7db0bbe1415b4d297908afe5c0b99552a
DouglasG04 <dg367077@gmaail.com>
2024-11-10 20:15:29 -0600

    Implement directives v18 HTML

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,28 +1,21 @@
 import { Component } from '@angular/core';
 import { faBars, faClose, faEnvelope, faDownload } from '@fortawesome/free-solid-svg-icons';
-import { faDatabase } from '@fortawesome/free-solid-svg-icons';
-import { faGithub, faLinkedin, faAngular, faHtml5, faCss3Alt, faJs, faGit, faBootstrap } from '@fortawesome/free-brands-svg-icons';
-import { Experience, Projects, TecnologiesSkills, WorkExperience } from 'src/app/interfaces/icons.interface';
-
-
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { Projects, TecnologiesSkills, WorkExperience } from 'src/app/interfaces/icons.interface';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-
-
-
 export class HomeComponent {
-
-  navbarOpenMobile = faBars
-  navBarCloseMobile = faClose
-  gitHub = faGithub
-  linkedin = faLinkedin
-  mail = faEnvelope
-  pdf = faDownload
-
+  
+  readonly navbarOpenMobile = faBars;
+  readonly navBarCloseMobile = faClose;
+  readonly gitHub = faGithub;
+  readonly linkedin = faLinkedin;
+  readonly mail = faEnvelope;
+  readonly pdf = faDownload;
 
   projects: Projects[] = [
     {
@@ -35,7 +28,7 @@ export class HomeComponent {
     {
       img: 'assets/project3.jpg',
       title: 'Kuppa',
-      description: 'Kuppa is a website of a fictitious company created at my university to solicit tourist services for a fictitious company.',
+      description: 'Kuppa is a website of a fictitious company created at my university to simulate a request for tourist services.',
       icons: ['assets/svg/css3.svg', 'assets/svg/javascript.svg', 'assets/svg/mongodb.svg'],
       links: ['https://github.com/DouglasG04/proyecto1-grupo2.git']
     },
@@ -53,121 +46,46 @@ export class HomeComponent {
       icons: ['assets/svg/vitejs.svg'],
       links: ['https://github.com/DouglasG04/mini-projects', 'https://todolistdgo.netlify.app/']
     },
-
-    {
-      img: 'assets/comming-soon.png',
-      title: 'Comming Soon',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae, odio.',
-      icons: ['assets/svg/clock.svg'],
-      links: ['https://github.com/DouglasG04/mini-projects']
-    }
-  ]
-
-
+  ];
 
   workExperience: WorkExperience[] = [
     {
       companyName: 'Tek Experts',
       position: 'Technical Support Engineer',
       period: '2023 - Present',
-      description: 'Collaborate and work with different departments to ensure user and business requirements. Communicating with Microsoft customers via phone and email. Interpret X++ code and use MS SQL Server scripts to solve their issues.',
+      description: 'Collaborate and work with different departments to ensure user and business requirements. Communicate with Microsoft customers via phone and email. Interpret X++ code and use MS SQL Server scripts to solve their issues.',
       technologies: [
-        {
-          icon: 'assets/svg/ssms.svg',
-          description: "SSMS logo"
-        },
-        {
-          icon: 'assets/svg/d365.svg',
-          description: "SSMS logo"
-        }
+        { icon: 'assets/svg/ssms.svg', description: 'SSMS logo' },
+        { icon: 'assets/svg/d365.svg', description: 'Dynamics 365 logo' }
       ]
     },
     {
       companyName: 'MAPFRE',
       position: 'Junior Developer',
       period: '2022 - July 2023',
-      description: 'Worked as a team with other developers and collaborated with other departments to ensure that the systems met user and business requirements. Use  SCRUM framework and agile methodologies to manage projects efficiently.',
+      description: 'Collaborated with developers and other departments to ensure systems met user and business requirements using the SCRUM framework and agile methodologies.',
       technologies: [
-        {
-          icon: 'assets/svg/html5.svg',
-          description: "HTML5 logo"
-        },
-        {
-          icon: 'assets/svg/css3.svg',
-          description: "CSS3 logo"
-        },
-        {
-          icon: 'assets/svg/csharp.svg',
-          description: "C Sharp logo"
-        },
-        {
-          icon: 'assets/svg/javascript.svg',
-          description: "JavaScript logo"
-        },
-        {
-          icon: 'assets/svg/angular.svg',
-          description: "Angular logo"
-        },
-        {
-          icon: 'assets/svg/tailwind-css.svg',
-          description: "Oracle logo"
-        },
-        {
-          icon: 'assets/svg/bootstrap.svg',
-          description: "Git logo"
-        },
-        {
-          icon: 'assets/svg/oracle.svg',
-          description: "Oracle logo"
-        },
-        {
-          icon: 'assets/svg/git.svg',
-          description: "Git logo"
-        },
-
+        { icon: 'assets/svg/html5.svg', description: 'HTML5 logo' },
+        { icon: 'assets/svg/css3.svg', description: 'CSS3 logo' },
+        { icon: 'assets/svg/csharp.svg', description: 'C# logo' },
+        { icon: 'assets/svg/javascript.svg', description: 'JavaScript logo' },
+        { icon: 'assets/svg/angular.svg', description: 'Angular logo' },
+        { icon: 'assets/svg/tailwind-css.svg', description: 'Tailwind CSS logo' },
+        { icon: 'assets/svg/bootstrap.svg', description: 'Bootstrap logo' },
+        { icon: 'assets/svg/oracle.svg', description: 'Oracle logo' },
+        { icon: 'assets/svg/git.svg', description: 'Git logo' },
       ]
     },
-  ]
-  work: any;
+  ];
 
   skills: TecnologiesSkills[] = [
-
-    {
-      icon: 'assets/svg/astro.svg',
-      description: "Astro",
-    },
-    {
-      icon: 'assets/svg/tailwind-css.svg',
-      description: "Tailwind CSS"
-    },
-
-    {
-      icon: 'assets/svg/javascript.svg',
-      description: "JavaScript"
-    },
-    {
-      icon: 'assets/svg/csharp.svg',
-      description: "C Sharp"
-    },
-    {
-      icon: 'assets/svg/angular.svg',
-      description: "Angular"
-    },
-    {
-      icon: 'assets/svg/react.svg',
-      description: "React"
-    },
-    {
-      icon: 'assets/svg/oracle.svg',
-      description: "PL SQL Oracle"
-    },
-    {
-      icon: 'assets/svg/git.svg',
-      description: "Git"
-    },
-  ]
+    { icon: 'assets/svg/astro.svg', description: 'Astro' },
+    { icon: 'assets/svg/tailwind-css.svg', description: 'Tailwind CSS' },
+    { icon: 'assets/svg/javascript.svg', description: 'JavaScript' },
+    { icon: 'assets/svg/csharp.svg', description: 'C#' },
+    { icon: 'assets/svg/angular.svg', description: 'Angular' },
+    { icon: 'assets/svg/react.svg', description: 'React' },
+    { icon: 'assets/svg/oracle.svg', description: 'PL SQL Oracle' },
+    { icon: 'assets/svg/git.svg', description: 'Git' }
+  ];
 }
-
-
-
-
